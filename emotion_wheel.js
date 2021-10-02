9d4024383fd28896d197f493cd8f7bbec68b0134
christophebrucker <christophe.brucker@gmail.com>
2021-10-02 21:27:31 +0200

    Hello

--- emotion_wheel.js
+++ emotion_wheel.js
@@ -3,6 +3,7 @@ const wheel = document.createElement('img');
 wheel.src = 'wheel.png';
 document.body.appendChild(wheel);
 
+//change pointer when mouse is over the wheel
 wheel.addEventListener("mouseover", (event) => {
   document.body.style.cursor = "url(http://bringerp.free.fr/Files/RotMG/cursor.gif), pointer"
 });
