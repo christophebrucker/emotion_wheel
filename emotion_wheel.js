0ff9364b131aa4630a91a0b67ea19f7f7acad57c
christophebrucker <christophe.brucker@gmail.com>
2021-10-19 18:45:36 +0200

    wheel

--- emotion_wheel.js
+++ emotion_wheel.js
@@ -1,9 +1,69 @@
-document.body.insertAdjacentHTML("afterbegin", "<h1>Hello Chris</h1>");
+document.body.insertAdjacentHTML("afterbegin", "<h1>How do you feel ?</h1>");
 const wheel = document.createElement('img');
 wheel.src = 'wheel.png';
+wheel.style.width = '250' + 'px';
 document.body.appendChild(wheel);
 
 //change pointer when mouse is over the wheel !
 wheel.addEventListener("mouseover", (event) => {
   document.body.style.cursor = "url(http://bringerp.free.fr/Files/RotMG/cursor.gif), pointer"
 });
+
+// set emotions
+const emotions = {
+  // red
+  angry: { mistrust: ['exhaustion','resentment'],
+           shame: ['humiliation', 'embarrasment'],
+           jealous: ['indignant', 'bitter'],
+           mad: ['furious', 'rage'],
+           irritation: ['aggressive', 'hostile'],
+           frustration: ['tense', 'annoyance'],
+           distant: ['withdrawn', 'numb'],
+           critical: ['skeptical', 'dismissive']
+  },
+  // green
+  disgusted: { disapproval: ['judgement','shock'],
+             disdain: ['revulsion', 'yucky'],
+             sick: ['nausea', 'awful'],
+             repulsion: ['horror', 'hesitance']
+  },
+  // black
+  afraid: { nervous: ['tight', 'vulnerable'],
+            shaky: ['trembling', 'unstable'],
+            weak: ['hollow', 'empty'],
+            insecure: ['small', 'inferior'],
+            anxious: ['overwhelm','worry'],
+            scared: ['helpless', 'fearful']
+  },
+  // purple
+  bad: { boredom: ['absent','apathy'],
+        busy: ['buzzy','pressure'],
+        stress: ['overwhelm', 'out of control'],
+        tired: ['sleepy', 'blurry']
+  },
+  // orange
+  surprised: { shock: ['uncomfortable','dismay'],
+        confusion: ['unclear','dizzy'],
+        amazement: ['astonishment', 'awe'],
+        excitement: ['eager', 'energetic']
+  },
+  // yellow
+  happy: { playful: ['arousal', 'mischevious'],
+          content: ['free', 'joy'],
+          curiosity: ['interest', 'willingness'],
+          proud: ['strong','confident'],
+          acceptance: ['respect','importance'],
+          powerful: ['courageous','creative'],
+          care: ['peaceful','gratitude'],
+          trust: ['tenderness', 'safety'],
+          hope: ['openness','inspiration']
+  },
+  // dark blue
+  sad: { lonely: ['achy', 'longing'],
+        vulnerable: ['shaky', 'fragile'],
+        despair: ['grief','powerless'],
+        guilty: ['remorseful','shame'],
+        depression: ['heavy','weight'],
+        hurt: ['pain','disappointment']
+  },
+};
